64d8e0644b964acf1a4322dd6f9468362ee8dc09
FauxKiwi <sfn14ok04@gmail.com>
2021-02-12 21:29:42 +0100

    Added documentation for shared

--- src/main/java/malculator/shared/ASTNode.java
+++ src/main/java/malculator/shared/ASTNode.java
@@ -3,32 +3,58 @@ package malculator.shared;
 import malculator.utils.Either;
 
 public interface ASTNode {
+    /**
+     * Represents the whole calculation
+     */
     class Calculation implements ASTNode {
         public Expression calculation;
     }
 
+    /**
+     * Represents an expression
+     */
     class Expression implements ASTNode {
         public Either<PrimaryExpression, Sum> value;
     }
 
+    /**
+     * Represents a sum
+     * children[0] operators[0] children[1] operators[1] ... children[n-1] operators[n-1] children[n]
+     * operators is 1 smaller than children
+     */
     class Sum implements ASTNode {
         public Product[] children;
         public Token.SumOp[] operators;
     }
 
+    /**
+     * Represents a product
+     * children[0] operators[0] children[1] operators[1] ... children[n-1] operators[n-1] children[n]
+     * operators is 1 smaller than children
+     */
     class Product implements ASTNode {
         public Power[] children;
         public Token.ProductOp[] operators;
     }
 
+    /**
+     * Represents a power
+     * the children are separated by ^
+     */
     class Power implements ASTNode {
         public PrimaryExpression[] children;
     }
 
+    /**
+     * A value or a parenthesized expression
+     */
     class PrimaryExpression implements ASTNode {
         public Either<Value, Expression> value;
     }
 
+    /**
+     * A number with signs
+     */
     class Value implements ASTNode {
         public Token.Number number;
         public Token.SumOp[] signs;
--- src/main/java/malculator/shared/Token.java
+++ src/main/java/malculator/shared/Token.java
@@ -1,14 +1,23 @@
 package malculator.shared;
 
 public interface Token {
+    /**
+     * A number
+     */
     class Number implements Token {
         public double value;
     }
 
+    /**
+     * + or -
+     */
     enum SumOp implements Token {
         PLUS, MINUS
     }
 
+    /**
+     * * or /
+     */
     enum ProductOp implements Token {
         TIMES, DIV
     }
